91b3f5909017d9cbb0bc69bd4fa91ebc8cb8f9d4
Megan Cullins <MCullins@jamarshall.com>
2019-12-10 11:37:47 -0600

    added pictures

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Killer Awesome Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images-na.ssl-images-amazon.com/images/I/81zkEZ%2B2SwL._SL1500_.jpg';
+  image3 = 'https://robots.nu/img/uploads/2019/02/28/little-robot-avatar.png';
 
   constructor() { }
 
